76e14df0b445be38f7259e64223682edc31813f3
bassy1992 <wyarquah@gmail.com>
2024-09-23 15:49:07 -0700

    project ini commit

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('store/', views.store, name='store'),
+    path('store/category/<slug:category_slug>/', views.store, name='products_by_category'),
+    path('store/category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+]
