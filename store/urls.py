f735013e8b0985679e792de87f55a86b50d60946
bassy1992 <wyarquah@gmail.com>
2024-09-26 08:01:38 -0700

    update

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('store/', views.store, name='store'),
     path('store/category/<slug:category_slug>/', views.store, name='products_by_category'),
     path('store/category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('search/', views.search, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -4,6 +4,9 @@ from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
+
 
 def store(request, category_slug=None):
     categories = None
@@ -30,6 +33,8 @@ def store(request, category_slug=None):
     }
     return render(request, 'store/store.html', context)
 
+
+
 def product_detail(request, category_slug, product_slug):
     single_product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
     in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
@@ -39,6 +44,29 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+
+
+
+
+def search(request):
+    products = []
+    products_count = 0
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword)
+            )
+            products_count = products.count()
+    context = {
+        'products': products,
+        'products_count': products_count,
+    }   
+    return render(request, 'store/store.html', context)
+
+
 
 
 
